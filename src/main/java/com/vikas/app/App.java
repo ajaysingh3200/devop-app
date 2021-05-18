3f044a9f69f6f72c7895d3fae862c382995aceed
ajaysingh3200 <66829002+ajaysingh3200@users.noreply.github.com>
2021-05-18 11:48:35 +0100

    Update name from vikas to ajay

--- src/main/java/com/vikas/app/App.java
+++ src/main/java/com/vikas/app/App.java
@@ -1,4 +1,4 @@
-package com.vikas.app;
+package com.ajay.app;
 
 /**
  * Hello world!
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World from jenkins" );
+        System.out.println( "Hello World from jenkins." );
     }
 }
